96358609b35f1ae478eebf4921ecf85e9dd61176
zhangweijuan <1761408621@qq.com>
2020-11-17 14:27:13 +0800

    初始化代码

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: zhangweijuan 
+ * @Date: 2020-11-10 15:15:53
+ * @LastEditTime: 2020-11-10 16:54:23
+ * @LastEditors: Please set LastEditors
+ * @Description: 入口
+ * @FilePath: \wei-go\app.js
+ */
+//app.js
+App({
+  //onLaunch,onShow: options(path,query,scene,shareTicket,referrerInfo(appId,extraData))
+  onLaunch: function(options){
+    
+  },
+  onShow: function(options){
+
+  },
+  onHide: function(){
+
+  },
+  onError: function(msg){
+
+  },
+  //options(path,query,isEntryPage)
+  onPageNotFound: function(options){
+
+  }
+});
